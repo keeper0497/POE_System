9674aec3f32ed249fd393bb11fb5f4d21ddac2a3
unknown <jhonreyartuz01@gmail.com>
2024-10-16 10:42:07 +0800

    update the settings database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,7 +121,10 @@ DATABASES = {
     #     "HOST": "127.0.0.1",
     #     "PORT": "5432",
     # }
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        engine='django.contrib.gis.db.backends.postgis'
+    )
 }
 
 
